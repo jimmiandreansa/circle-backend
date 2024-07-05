d6e6d6b8856df091bdeebf143e8bb83bc57aaf53
jimmiandreansa <jimmiandreansa11@gmail.com>
2024-07-05 15:24:43 +0700

    update: Images

--- src/middlewares/uploads.ts
+++ src/middlewares/uploads.ts
@@ -7,7 +7,7 @@ import path from "path";
 cloudinaryConfig();
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./src/uploads");
+    cb(null, "./src/images");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
